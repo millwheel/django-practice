358280da07987396cde1c6cdf4fd75c37e2cf1df
season <sionwer4@naver.com>
2023-08-25 11:02:03 +0900

    Add app detail view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from app.models import Post
 
 
 def index(request: HttpRequest) -> HttpResponse:
     qs = [{"id": 1, "title": "post #1"}]
     return render(request, "app/index.html", {"post_list": qs})
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    return render(
+        request,
+        "app/post_detail.html",
+        {
+            "post": post,
+        },
+    )
