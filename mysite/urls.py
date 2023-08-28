82cc41b8f8ad61c844f1e1d53c5a3345c22d85a9
season <sionwer4@naver.com>
2023-08-28 15:19:47 +0900

    Add account app

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views as app_views
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", app_views.index),
     path("app/<int:pk>", app_views.post_detail),
+    path("accounts/", include("accounts.urls")),
 ]
